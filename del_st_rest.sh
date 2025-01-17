b3cc08753e2fa320bd56343bd358b332b0868812
Joshua Pan <jp872@cornell.edu>
2025-01-17 18:13:56 +0000

    corrected further dangerous errors after testing

--- del_st_rest.sh
+++ del_st_rest.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-ARCHREST="/glade/derecho/scratch/archive/jpan/CASENAME/rest"
+set -e
+
+ARCHREST="/glade/derecho/scratch/jpan/archive/CASENAME/rest"
 
 cd $ARCHREST
 
-if [ "$(pwd)" == "$ARCHREST" ]; then
+if [ "$(pwd)" != "$ARCHREST" ]; then
   echo "Not running in the correct directory. Exiting to prevent rm -rf destroying things."
   exit 1
 fi
@@ -14,6 +16,6 @@ find . -type d -not -name "*-01-00000" | while read -r folder; do
   folder_name=$(basename "$folder")
   if [[ "$folder_name" =~ ^[0-9]{4}-[0-9]{2}-[0-9]{2}-[0-9]{5}$ ]]; then
     echo "Removing: $folder"  # Uncomment this line to see what will be removed
-    rm -rf "$folder"        # Uncomment this line to actually remove the folders
+    #rm -rf "$folder"        # Uncomment this line to actually remove the folders
   fi
 done
