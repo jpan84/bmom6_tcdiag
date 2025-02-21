f392a8805ab8a29b22fd95e33550e588ab9460a4
Joshua Pan <jvp5930@psu.edu>
2025-02-21 14:35:53 -0700

    changed layout to show u,v separately. added T

--- tc_before_after.py
+++ tc_before_after.py
@@ -11,9 +11,9 @@ import cartopy.crs as ccrs
 
 
 BBOX_DEG = 10.
-CASE = 'b.e23.BMOM.ne120np4_sx0.66av1.aqua.production.250218_unseed_all'
-OUTDIR = 'bef_aft.250218_unseed_all.contours/'
-seedlog = open('/glade/u/home/jpan/work/MOM6_CASEDIRS/250218_ne120np4_unseed_all.out2', 'r')
+CASE = 'b.e23.BMOM.ne120np4_sx0.66av1.aqua.production.250219_unseed_all'
+OUTDIR = 'bef_aft.250219_unseed_all/'
+seedlog = open('/glade/u/home/jpan/work/MOM6_CASEDIRS/250219_ne120np4_unseed_all.out', 'r')
 RESTDIR = '/glade/derecho/scratch/jpan/archive/%s/rest/%s/'
 grid = '/glade/p/cesmdata/inputdata/share/scripgrids/ne120np4_pentagons_100310.nc'
 ps_pattern = r"^\[(1\d{5}|9\d{4})\."
@@ -21,6 +21,8 @@ ps_pattern = r"^\[(1\d{5}|9\d{4})\."
 dpmin = 0 #3 #hPa
 dTmax = 0 #-1 #K
 CEN_DEG = 3 #find largest dp and dT within this many GCD of clon,clat
+
+Uclim = 20
 
 def main():
    print('\n\n\nStarting with dp, dT thresholds\n', dpmin, dTmax)
@@ -80,8 +82,13 @@ def main():
          oriu = selbbox(orids['U'].isel(lev=levi).isel(time=0))
          oriu = oriu - oriu.mean()
          oriv = selbbox(orids['V'].isel(lev=levi).isel(time=0))
+         oriv = oriv - oriv.mean()
          modu = selbbox(modds['U'].isel(lev=levi).isel(time=0))
+         modu = modu - oriu.mean()
          modv = selbbox(modds['V'].isel(lev=levi).isel(time=0))
+         modv = modv - oriv.mean()
+
+         oriT = selbbox(modds['T'].isel(lev=-9).isel(time=0))
 
          #TODO: maybe generalize to allow plotting other vars
          dp = float(selcir(difvar).max().values)
@@ -94,6 +101,38 @@ def main():
             continue
 
          #TODO: fix holoviews unable to plot xlim1 > xlim2 across the antimeridian
+         rastkwargs = dict(method='polygon', backend='bokeh')
+         framekwargs = dict(xlim=tuple(lonbnds), ylim=tuple(latbnds), aspect='square', frame_width=400)
+         plevs = np.arange(psmin // 2 * 2, 1042, 2)
+         panels = [[], []]
+
+         panels[0].append(difvar.plot.rasterize(**rastkwargs))
+         panels[0][-1] = panels[0][-1].opts(title='dp=%.2f' % dp, cmap='bwr', clim=(-dp, dp), symmetric=True, **framekwargs)
+         orip = orivar.plot.rasterize(**rastkwargs)
+         modp = modvar.plot.rasterize(**rastkwargs)
+
+         panels[0].append(oriu.plot.rasterize(**rastkwargs))
+         panels[0][-1] = panels[0][-1].opts(title='before u', cmap='BrBG', clim=(-Uclim, Uclim), symmetric=True, **framekwargs)
+         panels[0][-1] *= hvcontours(orip).opts(show_legend=False, **framekwargs)
+
+         panels[0].append(oriv.plot.rasterize(**rastkwargs))
+         panels[0][-1] = panels[0][-1].opts(title='before v', cmap='BrBG', clim=(-Uclim, Uclim), symmetric=True, **framekwargs)
+         panels[0][-1] *= hvcontours(orip).opts(show_legend=False, **framekwargs)
+
+         panels[1].append(oriT.plot.rasterize(**rastkwargs))
+         panels[1][-1] = panels[1][-1].opts(title='before T', cmap='jet', **framekwargs)
+         
+         panels[1].append(modu.plot.rasterize(**rastkwargs))
+         panels[1][-1] = panels[1][-1].opts(title='after u', cmap='BrBG', clim=(-Uclim, Uclim), symmetric=True, **framekwargs)
+         panels[1][-1] *= hvcontours(modp).opts(show_legend=False, **framekwargs)
+
+         panels[1].append(modv.plot.rasterize(**rastkwargs))
+         panels[1][-1] = panels[1][-1].opts(title='after v', cmap='BrBG', clim=(-Uclim, Uclim), symmetric=True, **framekwargs)
+         panels[1][-1] *= hvcontours(modp).opts(show_legend=False, **framekwargs)
+
+         layout = hv.Layout(panels[0] + panels[1]).cols(3)
+
+         '''
          prasts, urasts, vrasts = [], [], []
          prasts.append(difvar.plot.rasterize(method='polygon', backend='bokeh'))#, projection=ccrs.PlateCarree())
          #features = gf.coastline(projection=ccrs.PlateCarree(), line_width=1, scale='50m')
@@ -102,23 +141,32 @@ def main():
          #hv.save(prasts[-1], os.path.join(OUTDIR, 'difps_ux_%s_%s.png' % (tid, dtstr)))
 
          clim = (psmin // 5 * 5, 1020)
+         plevs = np.arange(psmin // 2 * 2, 1042, 2)
          prasts.append(orivar.plot.rasterize(method='polygon', backend='bokeh'))#, projection=ccrs.PlateCarree())
          #features = gf.coastline(projection=ccrs.PlateCarree(), line_width=1, scale='50m')
          prasts[-1] = prasts[-1].opts(cmap='BuPu', clim=clim, xlim=tuple(lonbnds), ylim=tuple(latbnds), aspect='square', frame_width=400)
          urasts.append(oriu.plot.rasterize(method='polygon', backend='bokeh'))
          urasts[-1] = urasts[-1].opts(cmap='BrBG', clim=(-20, 20), xlim=tuple(lonbnds), ylim=tuple(latbnds), aspect='square', frame_width=400, symmetric=True)
+         vrasts.append(oriv.plot.rasterize(method='polygon', backend='bokeh'))
+         vrasts[-1] = vrasts[-1].opts(cmap='PuOr', clim=(-20, 20), xlim=tuple(lonbnds), ylim=tuple(latbnds), aspect='square', frame_width=400, symmetric=True, alpha=0.6)
          #hv.save(prasts[-1], os.path.join(OUTDIR, 'orips_ux_%s_%s.png' % (tid, dtstr)))
 
          prasts.append(modvar.plot.rasterize(method='polygon', backend='bokeh'))#, projection=ccrs.PlateCarree())
          #features = gf.coastline(projection=ccrs.PlateCarree(), line_width=1, scale='50m')
          prasts[-1] = prasts[-1].opts(cmap='BuPu', clim=clim, xlim=tuple(lonbnds), ylim=tuple(latbnds), aspect='square', frame_width=400)
+         urasts.append(modu.plot.rasterize(method='polygon', backend='bokeh'))
+         urasts[-1] = urasts[-1].opts(cmap='BrBG', clim=(-20, 20), xlim=tuple(lonbnds), ylim=tuple(latbnds), aspect='square', frame_width=400, symmetric=True)
+         vrasts.append(modv.plot.rasterize(method='polygon', backend='bokeh'))
+         vrasts[-1] = vrasts[-1].opts(cmap='PuOr', clim=(-20, 20), xlim=tuple(lonbnds), ylim=tuple(latbnds), aspect='square', frame_width=400, symmetric=True, alpha=0.6)
          #hv.save(prasts[-1], os.path.join(OUTDIR, 'modps_ux_%s_%s.png' % (tid, dtstr)))
 
          #!TEST-CONTOURS
          cntrs = [hvcontours(rast).opts(color='black', show_legend=False, aspect='square', frame_width=400) for rast in prasts]
-         cntrs[1] = urasts[0] * cntrs[1]
+         cntrs[1] = urasts[0] * vrasts[0] * cntrs[1]
+         cntrs[2] = urasts[1] * vrasts[1] * cntrs[2]
 
-         layout = hv.Layout(cntrs)#hv.Layout([el[0] * el[1] for el in zip(prasts, cntrs)])
+         layout = hv.Layout([prasts[0], cntrs[1], cntrs[2]])#hv.Layout([el[0] * el[1] for el in zip(prasts, cntrs)])
+         '''
          hv.save(layout, os.path.join(OUTDIR, 'layout_ps_ux_%s_%s.png' % (tid, dtstr)))
 
          mkplt = False
