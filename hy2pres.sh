0ca3a7b41910946dfd0ec5d345081f3fd0552a72
Joshua Pan <jvp5930@psu.edu>
2025-04-29 12:28:56 -0600

    use ncremap for interp to plevs

---
+++ hy2pres.sh
@@ -0,0 +1,25 @@
+#!/bin/bash -l
+
+set -e
+module load nco
+
+cd /glade/derecho/scratch/jpan/archive/b.e23.BMOM.ne120np4_sx0.66av1.aqua.production.250415_unseed/atm
+pwd
+
+diri="./hist_regrid/"
+diro="./hist_regrid_onpres/"
+tapes=("h0a")
+vrt_in="/glade/u/home/jpan/L26_hyb.nc"
+vrt_out="/glade/u/home/jpan/L26_plevs.nc"
+
+if [ ! -d "$diro" ]; then
+  mkdir -p "$diro"
+fi
+
+for tp in "${tapes[@]}"; do
+  for nc in "${diri}"/*."${tp}".*.nc; do
+    echo $nc
+    bn=$(basename "${nc}")
+    ncremap --no_stdin --thr_nbr=1 --vrb=2 --vrt_in="${vrt_in}" --vrt_out="${vrt_out}" --ps_nm="${nc}"/PS "${nc}" "${diro}"/"${bn}"
+  done
+done
