fixed T tend plot pressure units, started working on unseed log parsing

atmo_ener.py
@@ -22,10 +22,22 @@ print(sys.argv)
 #DIRIDX = int(sys.argv[1])
 DIFF = bool(int(sys.argv[1])) #False if len(sys.argv) < 3 else bool(sys.argv[2])
 
-SFFILI = ['b.e23.BMOM.ne120np4_sx0.66av1.aqua.production.250417_ctrl__TEM.nc', 'b.e23.BMOM.ne120np4_sx0.66av1.aqua.production.250417_ctrl_b.e23.BMOM.ne120np4_sx0.66av1.aqua.production.250416_seed1x1_TEM.nc']
+SFFILI = ['b.e23.BMOM.ne120np4_sx0.66av1.aqua.production.250417_ctrl__TEM.nc',\
+          'b.e23.BMOM.ne120np4_sx0.66av1.aqua.production.250417_ctrl_b.e23.BMOM.ne120np4_sx0.66av1.aqua.production.250416_seed1x1_TEM.nc']
+         #'b.e23.BMOM.ne120np4_sx0.66av1.aqua.production.250417_ctrl_b.e23.BMOM.ne120np4_sx0.66av1.aqua.production.250415_unseed_TEM.nc']
 H0As = ['/glade/derecho/scratch/jpan/archive/b.e23.BMOM.ne120np4_sx0.66av1.aqua.production.250417_ctrl/atm/hist_regrid_0.25x0.25_onpres/*h0a*.nc',\
        '/glade/derecho/scratch/jpan/archive/b.e23.BMOM.ne120np4_sx0.66av1.aqua.production.250416_seed1x1/atm/hist_regrid_0.25x0.25_onpres/*h0a*.nc']
-ALIASES = ['ctrl', 'seed']
+       #'/glade/derecho/scratch/jpan/archive/b.e23.BMOM.ne120np4_sx0.66av1.aqua.production.250415_unseed/atm/hist_regrid_0.25x0.25_onpres/*h0a*.nc']
+
+
+'''
+SFFILI = ['b.e23.BMOM.ne120np4_sx0.66av1.aqua.production.250417_ctrl__TEM.nc',\
+          'b.e23.BMOM.ne120np4_sx0.66av1.aqua.production.250417_ctrl_b.e23.BMOM.ne120np4_sx0.66av1.aqua.production.250415_unseed_TEM.nc']
+H0As = ['/glade/derecho/scratch/jpan/archive/b.e23.BMOM.ne120np4_sx0.66av1.aqua.production.250417_ctrl/atm/hist_regrid_0.25x0.25_onpres/*h0a*.nc',\
+        '/glade/derecho/scratch/jpan/archive/b.e23.BMOM.ne120np4_sx0.66av1.aqua.production.250415_unseed/atm/hist_regrid_0.25x0.25_onpres/*h0a*.nc']
+'''
+       
+ALIASES = ['ctrl', 'seed'] #'un/seed'
 DENSFILI = 'tcdens.nc'
 
 PSI_NAME = 'PSI_resid'
@@ -56,7 +68,11 @@ def main():
    CS1 = ax.contour(np.sin(np.deg2rad(pltsf.lat)), pltsf[pres_name], pltsf.values / 10**expo, **contourkwargs) 
    ax.set_ylabel('Pressure [hPa]')
    ax.set_yticks(np.arange(100, 1001, 100))
-   plt.colorbar(CSF, cax=axes[0, 1])
+   cb = plt.colorbar(CSF, cax=axes[0, 1])
+   cbt = cb.get_ticks()
+   cbt = np.concatenate((cbt[:cbt.size // 2 + 1], -cbt[:cbt.size // 2 + 1][::-1]))
+   cb.set_ticks(cbt)
+   cb.set_ticklabels([(('%d' % t) if abs(t) >= 1 else t) for t in cbt])
    ax.set_xticks(np.sin(np.deg2rad(LATLAB)), labels=LATLAB.astype(np.int_))
    ax.set_yscale('log')
    #ax.set_xlim(np.sin(np.deg2rad(-15)), np.sin(np.deg2rad(90)))
@@ -65,8 +81,6 @@ def main():
    ax.yaxis.set_minor_formatter(mticker.ScalarFormatter())
    ax.yaxis.set_major_formatter(mticker.ScalarFormatter())
    #ax.clabel(CS1, **clabelkwargs)
-   ax.set_title('$\\bar{\Psi}^*$ (10$^{%d}$ kg s$^{-1}$)' % expo)
-   ax.set_xlabel('Latitude [°]')
 
    #plt.show()
    plt.savefig(os.path.join(DIRIO, '%s_%s_TEM_ener_tracks.png' % (ALIASES[0], ALIASES[1] if DIFF else '')), bbox_inches='tight')
@@ -77,21 +91,28 @@ def main():
       buddss[1].close()
 
    Tdot3D = sum([budds[dv] for dv in DIAB_VARS])
-   contourkwargs = {'colors': 'red', 'levels': np.arange(5e-2, 5.1e-1, 5e-2)}
+   contourkwargs = {'colors': 'lime', 'levels': np.arange(1e-1, 1.01, 1e-1) / (10 if DIFF else 1)}
    contourkwargs['levels'] = np.concatenate((-contourkwargs['levels'][::-1], contourkwargs['levels']))
-   CS2 = ax.contour(np.sin(np.deg2rad(Tdot3D.lat)), Tdot3D[pres_name], Tdot3D.values * 86400, **contourkwargs)
+   CS2 = ax.contour(np.sin(np.deg2rad(Tdot3D.lat)), Tdot3D[pres_name] / 100, Tdot3D.values * 86400, **contourkwargs)
+   ax.set_title('$\\bar{\Psi}^*$ (10$^{%d}$ kg s$^{-1}$)        %s: CI %.2f K day$^{-1}$' % (expo, contourkwargs['colors'], contourkwargs['levels'][-1] - contourkwargs['levels'][-2]))
+
+   debug_Tdot = xr.Dataset(data_vars=dict(Tdot3D=Tdot3D * 86400))
+   debug_Tdot.to_netcdf(os.path.join(DIRIO, 'debug_Tdot3D.nc'))
 
    plt.savefig(os.path.join(DIRIO, '%s_%s_TEM_ener_tracks.png' % (ALIASES[0], ALIASES[1] if DIFF else '')), bbox_inches='tight')
    #plt.show()
 
    integ3d = c.cp / c.g * Tdot3D.integrate(pres_name) #since cumulative not needed trapint(Tdot3D, Tdot3D[pres_name]).isel({pres_name: -1})
    sfcexch = budds['SHFLX'] - budds['LHFLX']
-   sumbud = integ3d + sfc
+   sumbud = integ3d + budds['SHFLX'] #sfcexch
+   latwgts = np.cos(np.deg2rad(sumbud.lat))
+   glob3d = (integ3d * latwgts).sum() / latwgts.sum()
+   globbud = (sumbud * latwgts).sum() / latwgts.sum()
 
    ax = axes[1, 0]
    ax.plot(np.sin(np.deg2rad(sfcexch.lat)), sfcexch.values, label='sfc')
-   ax.plot(np.sin(np.deg2rad(integ3d.lat)), integ3d.values, label='3D cond+lw+sw')
-   ax.plot(np.sin(np.deg2rad(sumbud.lat)), sumbud.values, label='sum')
+   ax.plot(np.sin(np.deg2rad(integ3d.lat)), integ3d.values, label='3D cond+lw+sw=%.2f W m$^{-2}$' % glob3d.values)
+   ax.plot(np.sin(np.deg2rad(sumbud.lat)), sumbud.values, label='3D+SH=%.2f W m$^{-2}$' % globbud.values, color='gray')
    ax.hlines(0, -1, 1, color='black', linestyle='--')
    ax.legend(framealpha=0.5)
    ax.set_title('Atmo energy budget [W m$^{-2}$]')
@@ -100,6 +121,7 @@ def main():
    #plt.show()
 
    densds = xr.open_dataset(os.path.join(DIRIO, DENSFILI))
+   densnorm = densds.ace.attrs['norm']
    if DIFF:
       densds = densds.sel(run=ALIASES[1]) - densds.sel(run=ALIASES[0])
    else:
@@ -112,7 +134,8 @@ def main():
    ax1.plot(np.sin(np.deg2rad(densds.lat)), densds.h6hurr.values, label='6h hurricane fixes', color='orange')
    ax1.set_ylabel('6h hurricane fixes')
    ax1.legend(framealpha=0.5, loc=1)
-   ax.set_title(densds.ace.attrs['norm'])
+   ax.set_title(densnorm)
+   ax.set_xlabel('Latitude [°]')
    ax.legend(framealpha=0.5, loc=2)
    ax.hlines(0, -1, 1, color='black', linestyle='--')
    ax1.hlines(0, -1, 1, color='black', linestyle='--')