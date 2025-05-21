b8a99f38e70e7f8351d40baccfee177fc9900738
Joshua Pan <jvp5930@psu.edu>
2025-05-21 10:19:29 -0600

    try avoiding OOM by splitting ltm computation into complete years

--- mon_2_ltm.py
+++ mon_2_ltm.py
@@ -6,20 +6,28 @@ import sys
 DIR = '/glade/derecho/scratch/jpan/archive/b.e23.BMOM.ne120np4_sx0.66av1.aqua.production.250417_ctrl/atm/hist_regrid_0.25x0.25_onpres'
 
 ds = xr.open_mfdataset(os.path.join(DIR, '*.h0a.*.nc'))
-print(ds.time.dt.days_in_month.data)
+#print(ds.time.dt.days_in_month.data)
 dpm = ds.time.dt.days_in_month.data
 wgts = dpm / dpm.sum()
 
-for dv in ds.data_vars:
-   if ds[dv].dims[0] == 'time' and np.issubdtype(ds[dv].dtype, np.number):
-      print('Averaging', dv)
-      ltm = np.einsum('i,i...->...', wgts, ds[dv]) #output of to_dataarray() has 'variable' as new 1st dimension. CESM output has time as 1st dimension.
-      ltm = xr.DataArray(ltm, coords={dm: ds[dm] for dm in ds[dv].dims[1:]}, attrs=ds[dv].attrs)
-      ds = ds.assign(variables={dv: ltm})
-   else:
-      print('Skipping', dv)
+fullyrs = wgts.size // 12
 
-ds.attrs['history'] = sys.argv[0] + ';\n' + ds.attrs['history']
-ds.attrs['ltm_start_date'] = str(ds.time.values[0])
-ds.attrs['ltm_end_date'] = str(ds.time.values[-1])
-ds.to_netcdf(os.path.join(DIR, 'ltm.nc'))
+for ii in range(fullyrs):
+   tslc = slice(12 * ii, 12 * (ii + 1))
+   subds = ds.isel(time=tslc)
+   subwgts = wgts[tslc]
+
+   for dv in subds.data_vars:
+      if subds[dv].dims[0] == 'time' and np.issubdtype(subds[dv].dtype, np.number):
+         print('Averaging', dv)
+         ltm = np.einsum('i,i...->...', subwgts, subds[dv]) #output of to_dataarray() has 'variable' as new 1st dimension. CESM output has time as 1st dimension.
+         ltm = xr.DataArray(ltm, coords={dm: subds[dm] for dm in subds[dv].dims[1:]}, attrs=subds[dv].attrs)
+         ltm = ltm.assign_coords(coords=dict(time=subds.time.values[0]))
+         subds = subds.assign(variables={dv: ltm})
+      else:
+         print('Skipping', dv)
+   
+   subds.attrs['history'] = sys.argv[0] + ';\n' + subds.attrs['history']
+   subds.attrs['ltm_start_date'] = str(subds.time.values[0])
+   subds.attrs['ltm_end_date'] = str(subds.time.values[-1])
+   subds.to_netcdf(os.path.join(DIR, 'ltm_%d.nc' % ii))
