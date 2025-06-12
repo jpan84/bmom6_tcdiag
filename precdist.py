8e75c7b719e466fbfc02edf03f6bed9302ec4d2b
Joshua Pan <jvp5930@psu.edu>
2025-06-12 10:21:20 -0600

    drafted latitude binned precip cdfs

--- precdist.py
+++ precdist.py
@@ -8,9 +8,15 @@ GRIDFN = 'ne120np4_pentagons_100310.nc'
 HISTS = '/glade/derecho/scratch/jpan/archive/%s/atm/hist_0010_h1i/*.h1i.*.nc'
 CASES = ['b.e23.BMOM.ne120np4_sx0.66av1.aqua.production.250415_unseed', 'b.e23.BMOM.ne120np4_sx0.66av1.aqua.production.250417_ctrl', 'b.e23.BMOM.ne120np4_sx0.66av1.aqua.production.250416_seed1x1']
 ALIASES = ['UNSEED', 'CTRL', 'SEED']
+COLORS = ['blue', 'orange', 'green']
+LATBINS = None
+MARKERS = ['.', 'v', '1', 's', '*', '+']
 
-cdfs = [] #store cdf of each case
+cdfs = [] #store cdf of each case as list(tuple(prate, areawgt, quantile))
+cdfslat = [] #store cdf of each case as list(list(tuple(prate, areawgt, quantile, (lowerlat, upperlat))))
+#outer list indexed by case, inner list indexed by latitude bin
 CDFPKL = '/glade/work/jpan/PRECTbmom/prect_cdf_8.pkl'
+CDFPKLLAT = '/glade/work/jpan/PRECTbmom/prect_cdf_8_lat.pkl'
 CALCCDFS = False
 
 a = 6.371e6 #m
@@ -29,12 +35,14 @@ import pltsettings
 import matplotlib.pyplot as plt
 import matplotlib.colors as colors
 
-def main():
+LATCDF = bool(int(sys.argv[1]))
+
+def main_global():
    global cdfs
    pltsettings.set1()
 
    if CALCCDFS:
-      for case in CASES:
+      for case in CASES: 
          cdfs.append(compute_cdfs(HISTS % case))
    
          print('Pickling...')
@@ -52,6 +60,31 @@ def main():
    plt.savefig('PRECT_cdf_test.png')
 
    ratio_line(cdfs[2], cdfs[0], ALIASES[2], ALIASES[0], plt.gca())
+   plt.close()
+
+def main_lat():
+   global cdfslat
+   pltsettings.set1()
+
+   if CALCCDFS:
+      for case in CASES: 
+         cdfslat.append(compute_cdfs_lat(HISTS % case))
+   
+         print('Pickling...')
+         with open('/glade/work/jpan/PRECTbmom/prect_cdf_%d_lat.pkl' % int(-np.log10(thresh)), 'wb') as fl:
+            pickle.dump(cdfslat, fl)
+   else:
+      with open(CDFPKLLAT, 'rb') as fl:
+         cdfslat = pickle.load(fl)
+
+   ax = plt.axes()
+   for casei in range(len(ALIASES)):
+      for latj in [6, 7, 8, 10]:
+         cdftup = cdfslat[casei][latj]
+         pltcdf_line(cdftup, ALIASES[casei] + '_' + cdftup[3][0] + '_' + cdftup[3][1], ax=ax, marker=MARKERS[latj])
+
+   plt.legend(loc='upper center', bbox_to_anchor=(0.5, 1.15), ncol=4, prop=dict(size=8))
+   plt.savefig('PRECT_cdf_lat_test.png')
    plt.close()
 
 def compute_cdfs(histpath):
@@ -72,25 +105,30 @@ def compute_cdfs(histpath):
    return precsort, wgts, qtiles
 
 def compute_cdfs_lat(histpath, latbins=np.arange(-90, 90.1, 15)):
-   print('Computing CDFs', histpath)
+   print('Computing lat-binned CDFs', histpath)
    ds = ux.open_mfdataset(os.path.join(GRIDDIR, GRIDFN), histpath)
 
+   cdftups = []#precsort, wgts, qtiles = None, None, None
    for ii in range(latbins.size - 1):
-   llat, ulat = latbins[ii], latbins[ii + 1]
+      llat, ulat = latbins[ii], latbins[ii + 1]
+      print('\t lats %.1f %.1f' % (llat, ulat))
+      idx = np.where((ds.lat >= llat) & (ds.lat < ulat))[0]
 
-   prec1d = ds.PRECT.values.reshape(-1) #shape (time, n_face)
-   areas = (ds.uxgrid.face_areas * a**2).data.repeat(ds.time.shape[0])
+      prec1d = ds['PRECT'].isel(n_face=idx).data.reshape(-1) #shape (time, n_face)
+      areas = (ds['area'].isel(n_face=idx) * a**2).data.repeat(ds.time.size)
 
-   print('\tSorting...')
-   sorter = np.argsort(prec1d)
-   precsort = prec1d[sorter]
-   wgts = areas[sorter]
-   wherethresh = np.where(precsort > thresh)[0][0]
-   precsort = precsort[wherethresh:]
-   wgts = wgts[wherethresh:]
-   qtiles = np.cumsum(wgts) / sum(wgts)
+      print('\tSorting...')
+      sorter = np.argsort(prec1d)
+      precsort = prec1d[sorter]
+      wgts = areas[sorter]
+      wherethresh = np.where(precsort > thresh)[0][0]
+      precsort = precsort[wherethresh:]
+      wgts = wgts[wherethresh:]
+      qtiles = np.cumsum(wgts) / sum(wgts)
 
-   return precsort, wgts, qtiles
+      cdftups.append((precsort, wgts, qtiles, (llat, ulat)))
+
+   return cdftups
 
 #takes cdf tuple like compute_cdfs() output
 def pltcdf_line(cdftup, label, ax=None, **pltkwargs):
@@ -107,7 +145,7 @@ def pltcdf_line(cdftup, label, ax=None, **pltkwargs):
       ax.set_xlabel('Percentile')
       ax.set_ylabel('P rate [mm h$^{-1}$]')
 
-   ln = ax.plot(1 - cdftup[2], cdftup[0] * MPS2MMHR, label=label)
+   ln = ax.plot(1 - cdftup[2], cdftup[0] * MPS2MMHR, label=label, **pltkwargs)
    return ax, ln
 
 def ratio_line(cdftup1, cdftup2, alias1, alias2, ax):
@@ -117,8 +155,8 @@ def ratio_line(cdftup1, cdftup2, alias1, alias2, ax):
    for tile in rattiles:
       idx1 = np.where(cdftup1[2] >= 1 - tile)[0][0]
       idx2 = np.where(cdftup2[2] >= 1 - tile)[0][0]
-      p1 = cdftup1[idx1]
-      p2 = cdftup2[idx2]
+      p1 = cdftup1[0][idx1]
+      p2 = cdftup2[0][idx2]
       rats.append(p1 / p2)
    ax1 = ax.twinx()
    ax1.plot(rattiles, rats, linestyle='-.', color='black')
@@ -176,4 +214,7 @@ def ratio_line(cdftup1, cdftup2, alias1, alias2, ax):
    '''
 
 if __name__ == '__main__':
-   main()
+   if LATCDF:
+      main_lat()
+   else:
+      main_global()
