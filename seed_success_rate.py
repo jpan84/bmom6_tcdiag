0b98c9b547bbaddf7c8bb69ccb389cf190f511e2
Joshua Pan <jipan84@gmail.com>
2026-01-20 12:26:11 -0800

    basic dt selection and gcd working

---
+++ seed_success_rate.py
@@ -0,0 +1,48 @@
+import pandas as pd
+import numpy as np
+import sys
+import cftime
+import datetime as dt
+
+SEDFIL = sys.argv[1]
+PARFIL = sys.argv[2]
+dt_fmt = '%Y-%m-%d %H:%M:%S'
+
+def main():
+   sdf = pd.read_csv(SEDFIL)
+   pdf = pd.read_csv(PARFIL)
+   #print(sdf.head())
+   #print(pdf.head())
+
+   sdf['dt'] = sdf['dt'].apply(cftime.datetime.strptime, args=(dt_fmt,), calendar='noleap')
+   try:
+      pdf['dt'] = pdf['dt'].apply(cftime.datetime.strptime, args=(dt_fmt,), calendar='noleap')
+   except ValueError: #tc_stats post-processing accidentally interpolated leap days
+      pdf['dt'] = pdf['dt'].apply(cftime.datetime.strptime, args=(dt_fmt,), calendar='Gregorian')
+      #print(pdf['dt'].apply(lambda x: x.day))
+      pdf = pdf[~pdf['dt'].apply(lambda x: x.month == 2 and x.day == 29)]
+      pdf['dt'] = pdf['dt'].apply(lambda d: cftime.datetime(d.year, d.month, d.day, d.hour, calendar='noleap'))
+   pdf['dt'] = pdf['dt'] - dt.timedelta(days=2000 * 365)
+
+   for ix, rw in sdf.iterrows(): 
+      #print(type(rw['dt']))
+      dtmatch = pdf[pdf['dt'] == rw['dt']]
+      #print(np.deg2rad(dtmatch['lon']))
+      print(rw['clon'], rw['clat'])
+      print(dtmatch)
+      print(gcd_deg(rw['clon'], rw['clat'], dtmatch['lon'], dtmatch['lat']))
+      break
+
+def gcd_deg(clon, clat, ser_lon, ser_lat):
+   clonr, clatr = np.deg2rad(clon), np.deg2rad(clat)
+   ser_lonr, ser_latr = np.deg2rad(ser_lon), np.deg2rad(ser_lat)
+
+   dlat = ser_latr - clatr
+   dlon = ser_lonr - clonr
+
+   hav = np.sin(dlat / 2)**2 + np.cos(clatr) * np.cos(ser_latr) * np.sin(dlon / 2)**2
+   gcdr = 2 * np.arctan2(np.sqrt(hav), np.sqrt(1 - hav))
+   return np.rad2deg(gcdr)
+
+if __name__ == '__main__':
+   main()
