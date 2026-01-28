fixed signs for latitude selection, clip the haversine

plt_EP.py
@@ -5,15 +5,15 @@ import matplotlib.colors as colors
 import matplotlib.ticker as mticker
 import numpy as np
 
-DIRI = './streamf_sznl_0005-0015'
+DIRI = './streamf_sznl_seedmatch'
 FILI = 'b.e23.BMOM.ne120np4_sx0.66av1.aqua.production.250417_ctrl__TEM.nc'
 
 #DIRI = '/glade/derecho/scratch/jpan/archive/b.e23.BMOM.ne120np4_sx0.66av1.aqua.production.251010_ctrlbr/atm'
 #FILI = '0012_JJASON_onpres_0.25_EPF_tm.nc'
 DIFF = False
 
-#FILI = 'b.e23.BMOM.ne120np4_sx0.66av1.aqua.production.250417_ctrl_b.e23.BMOM.ne120np4_sx0.66av1.aqua.production.250416_seed1x1_TEM.nc'
-#DIFF = True
+FILI = 'b.e23.BMOM.ne120np4_sx0.66av1.aqua.production.250417_ctrl_b.e23.BMOM.ne120np4_sx0.66av1.aqua.production.251229_seedmatch_TEM.nc'
+DIFF = True
 
 YSCL = lambda lat: np.sin(np.deg2rad(lat))
 YLAB = np.arange(-90, 90.1, 30).astype(np.int_)

plt_track_dens.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 from sznl_funcs import stack_hemi_sznl
 
 FILI = '/glade/u/home/jpan/aquaptc/tempest/260112_density_sznl/tcdens.nc'
-SZN = 'JJA'
+SZN = 'SON'
 
 YSCL = lambda deglat: np.sin(np.deg2rad(deglat))
 LATLAB = np.arange(-90, 90.1, 15).astype(np.int_)
@@ -59,7 +59,7 @@ axes[0][1].annotate('', xy=(STZ[0], 2.35), xytext=(STZ[1], 2.35), arrowprops=dic
                         lw=1.5, color='#E4D00A', connectionstyle=f'bar,fraction={1/6}'), annotation_clip=False)
 axes[0][1].text(sum(STZ) / 2, 2.6, 'STZ', ha='center', va='bottom', fontsize=10, fontweight='bold', transform=axes[0][1].transData, c='#E4D00A')
 
-axes[0][2].set_title('SEED–CTRL')
+axes[0][2].set_title('MSEED–CTRL')
 axes[0][2].plot(sinlat, lysplt.isel(run=2) - lysplt.isel(run=1))
 axes[0][2].plot(sinlat, genplt.isel(run=2) - genplt.isel(run=1))
 axes[0][2].plot(sinlat, sdsplt, color='maroon', linestyle='dashdot', label='seed events')

seed_success_rate.py
@@ -9,9 +9,10 @@ PARFIL = sys.argv[2]
 LAT1 = float(sys.argv[3]) #lower bound (positive, inclusive)
 LAT2 = float(sys.argv[4]) #upper bound (positive, exclusive)
 dt_fmt = '%Y-%m-%d %H:%M:%S'
+print(sys.argv, '\n')
 
 lags = np.arange(6, 13, 6.)
-rngs = np.arange(1, 3.1, 0.5)
+rngs = np.arange(2, 3.1, 0.5)
 #lags = np.arange(6, 7, 6.)
 #rngs = np.arange(1, 3, 1.)
 tab1 = np.zeros((lags.size, rngs.size)) #count exactly one match
@@ -24,7 +25,7 @@ def main():
    #print(pdf.head())
 
    sdf['dt'] = sdf['dt'].apply(cftime.datetime.strptime, args=(dt_fmt,), calendar='noleap')
-   sdf = sdf[(sdf['clat'] >= LAT1) & (sdf['clat'] < LAT2) | (sdf['clat'] <= LAT1) & (sdf['clat'] > LAT2)]
+   sdf = sdf[(sdf['clat'] >= LAT1) & (sdf['clat'] < LAT2) | (sdf['clat'] <= -LAT1) & (sdf['clat'] > -LAT2)]
 
    pdf = pdf[pdf['isgen']]
    try:
@@ -49,7 +50,8 @@ def main():
       for jj, rg in enumerate(rngs):
          print('Working on lag, range', ll, rg)
          for ix, rw in sdf.iterrows():
-            print('\t', rw['dt'])
+            if rw['dt'].day == 1 and rw['dt'].hour == 0:
+               print('\t', rw['dt'])
             dtmatch = pdf[(pdf['dt'] - rw['dt'] <= dt.timedelta(hours=ll)) & (pdf['dt'] - rw['dt'] >= dt.timedelta(hours=lags[0]))]
             #print(rw['dt'])
             #print(dtmatch)
@@ -69,8 +71,10 @@ def gcd_deg(clon, clat, ser_lon, ser_lat):
 
    dlat = ser_latr - clatr
    dlon = ser_lonr - clonr
+   #dlon = (dlon + np.pi) % (2 * np.pi) - np.pi
 
    hav = np.sin(dlat / 2)**2 + np.cos(clatr) * np.cos(ser_latr) * np.sin(dlon / 2)**2
+   hav = np.clip(hav, 0, 1)
    gcdr = 2 * np.arctan2(np.sqrt(hav), np.sqrt(1 - hav))
    return np.rad2deg(gcdr)
 