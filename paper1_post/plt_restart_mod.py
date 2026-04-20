6132d96a53e1e4581df28798d5ed4e016dc8975b
Joshua Pan <jvp5930@psu.edu>
2026-04-20 11:33:15 -0600

    coded tangential wind but wrong

--- paper1_post/plt_restart_mod.py
+++ paper1_post/plt_restart_mod.py
@@ -8,12 +8,17 @@ import matplotlib.pyplot as plt
 RSDIR = '/glade/derecho/scratch/jpan/unseed_restarts_paper1/'
 EVNTS = '~/aquaptc/tempest/250415_unseed_production_unseed_events.parquet'
 GRIDF = '/glade/p/cesmdata/inputdata/share/scripgrids/ne120np4_pentagons_100310.nc'
+GRIDDELTA = 0.25
 
 DTSTR = '0005-08-07-00000'
 CLAT, CLON = 18.00722, 98.79274
+RADO = 5. #gcd
 
 MODSTR = '*%s.nc'
 ORISTR = '*%s.nc.ORIG.nc'
+
+PLEVS = levels=np.arange(-1e4, 0, 5e2)
+PLEVS = np.concatenate((PLEVS, -PLEVS[::-1]))
 
 def main():
    orids = ux.open_mfdataset(GRIDF, os.path.join(RSDIR, ORISTR % DTSTR)).expand_dims(state=['before'])
@@ -37,11 +42,16 @@ def main():
    #TODO: compute tangential wind
 
    true_ctr = ds.uxgrid.subset.nearest_neighbor((CLON, CLAT), 1)
-   az_mean = lambda da: da.azimuthal_mean((true_ctr.face_lon, true_ctr.face_lat), 5, 0.25)
+   az_mean = lambda da: da.azimuthal_mean((true_ctr.face_lon, true_ctr.face_lat), RADO, GRIDDELTA)
 
    test_p = mirror_azim_mean(az_mean(p_lev).isel(state=0).squeeze())
-   print(test_p)
-   plt.contour(test_p['radius'], lev_coord.isel(state=0), test_p, levels=np.arange(1.5e4, 9.6e4, 5e3), colors='black')
+   test_p -= test_p.isel(radius=-1)
+   test_vt = mirror_azim_mean(az_mean(uv_to_tang(ds, 'U', 'V', (CLON, CLAT), radbound=RADO)).isel(state=0).squeeze())
+   print(test_vt)
+   #plt.contour(test_p['radius'], lev_coord.isel(state=0), test_p, levels=np.arange(1.5e4, 9.6e4, 5e3), colors='black')
+   plt.contourf(test_vt['radius'], lev_coord.isel(state=0), test_vt, levels=np.arange(-50, 51, 5), cmap='PRGn')
+   plt.colorbar()
+   plt.contour(test_p['radius'], lev_coord.isel(state=0), test_p, levels=PLEVS, colors='black')
    plt.ylim(1000, 70)
    plt.yscale('log')
    plt.show()
@@ -50,6 +60,22 @@ def mirror_azim_mean(am):
    flip = am.isel(radius=slice(-1, None, -1))
    flip = flip.assign_coords(radius=-flip['radius'])
    return xr.concat([flip, am], dim='radius')
+
+def uv_to_tang(uxds, unm, vnm, ctrcoord, radbound=None):
+   myu, myv, mylat, mylon = uxds[unm], uxds[vnm], uxds['lat'], uxds['lon']
+   if radbound is not None:
+      bc = lambda da: da.subset.bounding_circle(ctrcoord, radbound + 1, element='face centers')
+      myu, myv, mylat, mylon = bc(myu), bc(myv), bc(mylat), bc(mylon)
+   clon, clat = ctrcoord
+   d1 = np.sin(np.deg2rad(clat)) * np.cos(np.deg2rad(mylat)) - np.cos(np.deg2rad(clat)) * np.sin(np.deg2rad(mylat)) * np.cos(np.deg2rad(mylon) - np.deg2rad(clon))
+   d2 = np.cos(np.deg2rad(clat)) * np.sin(np.deg2rad(mylon) - np.deg2rad(clon))
+   d = np.maximum(1e-25, np.sqrt(d1 ** 2. + d2 ** 2.))
+
+   ufac = d1 / d
+   vfac = d2 / d
+
+   return myv * vfac - myu * ufac
+
 
 if __name__ == '__main__':
    main()
