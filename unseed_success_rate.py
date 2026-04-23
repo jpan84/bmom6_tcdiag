fe9c59b0c881527724eb77e76977ce05c50e8f97
Joshua Pan <jvp5930@psu.edu>
2026-04-23 13:53:24 -0600

    added parquet to unseed success script. fixed lon wrapping

--- unseed_success_rate.py
+++ unseed_success_rate.py
@@ -33,13 +33,13 @@ def main():
    #sdf = sdf[(sdf['clat'] >= LAT1) & (sdf['clat'] < LAT2) | (sdf['clat'] <= -LAT1) & (sdf['clat'] > -LAT2)]
 
    try:
-      pdf['dt'] = pdf['dt'].apply(cftime.datetime.strptime, args=(dt_fmt,), calendar='noleap')
+      pdf['dt_obj'] = pdf['dt'].apply(cftime.datetime.strptime, args=(dt_fmt,), calendar='noleap')
    except ValueError: #tc_stats post-processing accidentally interpolated leap days
-      pdf['dt'] = pdf['dt'].apply(cftime.datetime.strptime, args=(dt_fmt,), calendar='Gregorian')
+      pdf['dt_obj'] = pdf['dt'].apply(cftime.datetime.strptime, args=(dt_fmt,), calendar='Gregorian')
       #print(pdf['dt'].apply(lambda x: x.day))
-      pdf = pdf[~pdf['dt'].apply(lambda x: x.month == 2 and x.day == 29)]
-      pdf['dt'] = pdf['dt'].apply(lambda d: cftime.datetime(d.year, d.month, d.day, d.hour, calendar='noleap'))
-   pdf['dt'] = pdf['dt'] - dt.timedelta(days=2000 * 365)
+      pdf = pdf[~pdf['dt_obj'].apply(lambda x: x.month == 2 and x.day == 29)]
+      pdf['dt_obj'] = pdf['dt_obj'].apply(lambda d: cftime.datetime(d.year, d.month, d.day, d.hour, calendar='noleap'))
+   pdf['dt_obj'] = pdf['dt_obj'] - dt.timedelta(days=2000 * 365)
    #pdf['istouched'] = False #flag for whether a discrete storm has been touched by unseed
    pdf['unseed_pt'] = False #flag for whether a fix is associated with an unseed event
 
@@ -47,7 +47,7 @@ def main():
    for ix, rw in sdf.iterrows():
       if rw['dt'].day == 1 and rw['dt'].hour == 0:
          print('\t', rw['dt'])
-      dtmatch = pdf[(pdf['dt'] - rw['dt'] <= dt.timedelta(hours=MAXLAG)) & (pdf['dt'] - rw['dt'] >= dt.timedelta(hours=MINLAG))] #allow for SN trajectory gap
+      dtmatch = pdf[(pdf['dt_obj'] - rw['dt'] <= dt.timedelta(hours=MAXLAG)) & (pdf['dt_obj'] - rw['dt'] >= dt.timedelta(hours=MINLAG))] #allow for SN trajectory gap
       #print(rw['dt'])
       #print(dtmatch)
       inrng = dtmatch[gcd_deg(rw['clon'], rw['clat'], dtmatch['lon'], dtmatch['lat']) <= RNG]
@@ -75,7 +75,9 @@ def main():
    #nstm = pdf.drop_duplicates(subset=['stmnum'], keep='first').shape[0]
    #print('storms touched by unseed:', tchd, '/', nstm)
 
+   pdf = pdf.drop(columns=['dt_obj'])
    pdf.to_csv(PARFIL + '.flagged.csv')
+   pdf.to_parquet(PARFIL + '.flagged.parquet')
 
 def gcd_deg(clon, clat, ser_lon, ser_lat):
    clonr, clatr = np.deg2rad(clon), np.deg2rad(clat)
@@ -83,7 +85,7 @@ def gcd_deg(clon, clat, ser_lon, ser_lat):
 
    dlat = ser_latr - clatr
    dlon = ser_lonr - clonr
-   #dlon = (dlon + np.pi) % (2 * np.pi) - np.pi
+   dlon = (dlon + np.pi) % (2 * np.pi) - np.pi
 
    hav = np.sin(dlat / 2)**2 + np.cos(clatr) * np.cos(ser_latr) * np.sin(dlon / 2)**2
    hav = np.clip(hav, 0, 1)
