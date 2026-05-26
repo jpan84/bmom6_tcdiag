f251b28cef8796c95bee1a3b80434d0c2dcb125e
Joshua Pan <jvp5930@psu.edu>
2026-05-26 12:28:14 -0600

    colorbars and maps set

--- paper1_post/plt_thermo_state.py
+++ paper1_post/plt_thermo_state.py
@@ -50,110 +50,115 @@ def main():
    othplt = agg_time([ds['thetao'] for ds in ods], latnm=OLAT)
    omlplt = agg_time([ds['oml'] for ds in ods], latnm=OLAT, diff=False)
 
-   ############### Plot atmo MSE,DSE above ocean theta,MLdepth #################################
-   #TODO: set contour levels
-   sf_cfkwargs = {'levels': 2.**np.arange(-2, 7, 1), 'norm': colors.SymLogNorm(2.**-1)}
-   sf_cfkwargs['levels'] = np.concatenate((-sf_cfkwargs['levels'][::-1], sf_cfkwargs['levels']))
-   sf_ctkwargs = {'colors': 'black', 'levels': 2.**np.arange(-2, 7, 1)}
-   sf_ctkwargs['levels'] = np.concatenate((-sf_ctkwargs['levels'][::-1], sf_ctkwargs['levels']))
-   
-   # Define symmetric ticks for the colorbars
-   pos_ticks = 2.**np.arange(0, 7, 2) # [1., 4., 16., 64.]
-   base_sym_ticks = np.concatenate((-pos_ticks[::-1], [0], pos_ticks))
-   
+   mse_kw = dict(levels=np.arange(-2000, 2001, 200), cmap='seismic')
+   dse_kw = dict(levels=mse_kw['levels'][mse_kw['levels'] != 0])
+   ctl_kw = dict(levels=np.arange(3e5, 4.01e5, 5e3), cmap='viridis')
+   olevs = np.arange(-.2, .21, .02)
+   olevs_c = np.arange(0, 40, 4)
+
    plt.rc('font', size=16)
-   plt.rcParams['figure.figsize'] = (24, 12)
-   
-   # --- NEW/MODIFIED CODE START: Unified GridSpec for spacing control ---
-   
-   # Total Grid Dimensions: 2 rows, 8 columns (3 Plot, 3 Cax, 2 Gap)
-   # [Plot, Cax, Gap, Plot, Cax, Gap, Plot, Cax]
-   gs_ratios = [1.3, 0.05, 0.5, 1.3, 0.05, 0.5, 1.3, 0.05] 
-   
-   fig = plt.figure(figsize=plt.rcParams['figure.figsize'])
-   gs = fig.add_gridspec(nrows=2, ncols=8, wspace=0.1, hspace=0.2,
-                         height_ratios=[1, 1],
-                         width_ratios=gs_ratios)
-   
-   #fig.suptitle('Atmosphere Eulerian mean mass streamfunction\
-                   #\nOcean Residual Mass Streamfunction')
-   
-   # --- NEW/MODIFIED CODE END ---
-   
-   #for ii, szn in enumerate(ocn_yz['season']):
-   #   for jj in range(len(ALIS)):
+   fig = plt.figure(figsize=(22, 13)) # Marginally wider aspect ratio to accommodate shared side colorbars or clean baselines
 
-   for rr in range(gs.nrows):
-      for cc in range(3): #remove the hardcoded 3
-         ix1d = rr * 3 + cc
-         ixh = IXHORS[ix1d]
-         col_start = cc * 3
-         sp = gs[rr, col_start]
+   # --- NEW GRIDSPEC ARCHITECTURE ---
+   # 2 Main Rows for data plots. 1 Tiny bottom row for shared anomaly colorbars.
+   # 3 Columns for your experiments. 1 Tiny right column for the Control absolute colorbars.
+   gs = fig.add_gridspec(nrows=3, ncols=4, 
+                         height_ratios=[1, 1, 0.08], 
+                         width_ratios=[1, 1, 1, 0.06],
+                         wspace=0.25, hspace=0.3)
 
-         # Create a GridSpecFromSubplotSpec to manage the tight vertical split (ax_top/ax_bot)
-         gs_vertical = sp.subgridspec(2, 1, height_ratios=[1, 0.5], hspace=0.0)
-         
-         # Add the subplots
-         ax_top = fig.add_subplot(gs_vertical[0, 0])
-         ax_bot = fig.add_subplot(gs_vertical[1, 0], sharex=ax_top)
-         
-         # Add the colorbar axes to the second column of the group (col_start + 1)
-         cax = fig.add_subplot(gs[rr, col_start + 1]) 
-         
-         # --- NEW: Set Thicker Borders (Spines) for ax_top and ax_bot ---
-         border_thickness = 2.0
-         for axis in [ax_top, ax_bot]:
-            for spine in axis.spines.values():
-                spine.set_linewidth(border_thickness)
-         # ---------------------------------------------------------------
-         
-   
-         # Determine correct symmetric ticks for the current expo
-         current_ticks = base_sym_ticks# * (1e10 / expo)
-   
-         csf = ax_bot.contourf(YSCL(othplt[ixh]['lat']), othplt[ixh]['zl'], othplt[ixh], cmap='magma' if ixh == CTLIX else 'bwr')#, **sf_cfkwargs)
-         
-         # --- MODIFIED: Use the newly created cax and defined ticks ---
-         #plt.colorbar(csf, label='[10$^{%d}$ kg/s]' % int(np.log10(expo)), 
-         #             cax=cax, pad=.01, ticks=current_ticks)
-         # -------------------------------------------------------------
-         
-         #ax_bot.contour(YSCL(ocn_yz['yq']), ZSCL(ocn_yz['zl']), sf / expo, **sf_ctkwargs)
-         ax_bot.plot(YSCL(omlplt[ixh]['lat']), omlplt[ixh])
-         ax_bot.set_xticks(YLOC, ['' if yl % 20 else yl for yl in YLAB]) # Keeps xtick labels on ax_bot
-         ax_bot.set_xlim(-1, 1)
-         ax_bot.set_ylim(0, 600)
-         #ax_bot.set_yticks(ZLOC, ZLAB)
-         ax_bot.invert_yaxis()
-         ax_bot.set_xlabel('Latitude [°]')
-         ax_bot.set_ylabel('Depth [m]')
-         ax_bot.tick_params(right=True)
-         ax_bot.tick_params(length=7, width=1.3, which='both')
+   # Placeholders to capture mappables for colorbars
+   cf_atmo_ctl, cf_atmo_anom = None, None
+   cf_ocn_ctl, cf_ocn_anom = None, None
 
-         csf = ax_top.contourf(YSCL(mseplt[ixh]['latitudes']), mseplt[ixh]['lev'], mseplt[ixh])#, cmap='coolwarm' if jj == 1 else 'bwr', extend='both', **sf_cfkwargs)
-         ax_top.contourf(YSCL(dseplt[ixh]['latitudes']), dseplt[ixh]['lev'], dseplt[ixh])
-         #ax_top.contour(YSCL(temds['lat']), temds['plev'], temds['PSI_EM'].isel(case=1, season=ii) / cexpo, **sf_ctkwargs)
-         ax_top.set_ylim(1000, 100)
-         ax_top.set_yscale('log')
-         ax_top.yaxis.set_minor_formatter(mticker.ScalarFormatter())
-         ax_top.yaxis.set_major_formatter(mticker.ScalarFormatter())
-         ax_top.set_ylabel('Pressure [hPa]')
-         ax_top.set_yticks(np.arange(200, 1001, 200))
-         ax_top.tick_params(top=True, right=True, length=7, width=1.3, which='both')
-         ax_top.tick_params(which='minor', labelsize=0)
-   
-         label_index = (rr * 3) + cc
-         panel_label = f'({chr(97 + label_index)})'
-         ax_top.set_title(panel_label, loc='left')
+   for rr in range(2): # Process 2 rows of subplots
+      for cc in range(3): # Process 3 experiment columns
+          ix1d = rr * 3 + cc
+          ixh = IXHORS[ix1d]
+          
+          # Target the standard plot grid cell
+          sp = gs[rr, cc]
+          gs_vertical = sp.subgridspec(2, 1, height_ratios=[1, 0.5], hspace=0.05)
 
-         ax_top.set_title(TTLS[ixh], fontsize=24)
-         
-         # --- NEW/MODIFIED: Ensure no xtick labels on ax_top ---
-         ax_top.tick_params(labelbottom=False) 
-         # ax_top.set_xticklabels([]) is now redundant but keeping tick_params is cleaner
-         # -------------------------------------------------------
+          ax_top = fig.add_subplot(gs_vertical[0, 0])
+          ax_bot = fig.add_subplot(gs_vertical[1, 0], sharex=ax_top)
 
+          # --- Handle Spines & Styling ---
+          border_thickness = 2.0
+          for axis in [ax_top, ax_bot]:
+              for spine in axis.spines.values():
+                  spine.set_linewidth(border_thickness)
+
+          # --- Plot Ocean Data (Bottom Panel) ---
+          is_control = (ixh == CTLIX)
+          ocn_cmap = 'magma' if is_control else 'bwr'
+          
+          csf_ocn = ax_bot.contourf(YSCL(othplt[ixh]['lat']), othplt[ixh]['zl'], othplt[ixh], cmap=ocn_cmap, levels=olevs_c if is_control else olevs)
+          
+          # Cache mapping handles for later colorbar generation
+          if is_control:
+              cf_ocn_ctl = csf_ocn
+          elif cf_ocn_anom is None:
+              cf_ocn_anom = csf_ocn
+
+          ax_bot.plot(YSCL(omlplt[ixh]['lat']), omlplt[ixh], color='black', linewidth=1.5)
+          ax_bot.set_xticks(YLOC, ['' if yl % 20 else yl for yl in YLAB])
+          ax_bot.set_xlim(-1, 1)
+          ax_bot.set_ylim(0, 400)
+          ax_bot.invert_yaxis()
+          
+          if rr == 1: # Only label the true bottom row X-axis
+              ax_bot.set_xlabel('Latitude [°]')
+          else:
+              ax_bot.tick_params(labelbottom=False)
+              
+          ax_bot.set_ylabel('Depth [m]') if cc == 0 else ax_bot.tick_params(labelleft=False)
+
+          # --- Plot Atmosphere Data (Top Panel) ---
+          csf_atmo = ax_top.contourf(YSCL(mseplt[ixh]['latitudes']), mseplt[ixh]['lev'], mseplt[ixh], 
+                                     extend='both', **(mse_kw if not is_control else ctl_kw))
+
+          css_atmo = ax_top.contour(YSCL(dseplt[ixh]['latitudes']), dseplt[ixh]['lev'], dseplt[ixh], 
+                                     colors='lime', levels=(dse_kw if not is_control else ctl_kw)['levels'])
+          
+          if is_control:
+              cf_atmo_ctl = csf_atmo
+          elif cf_atmo_anom is None:
+              cf_atmo_anom = csf_atmo
+
+          ax_top.set_ylim(1000, 100)
+          ax_top.set_yscale('log')
+          ax_top.yaxis.set_minor_formatter(mticker.ScalarFormatter())
+          ax_top.yaxis.set_major_formatter(mticker.ScalarFormatter())
+          
+          ax_top.set_ylabel('Pressure [hPa]') if cc == 0 else ax_top.tick_params(labelleft=False)
+          ax_top.set_yticks(np.arange(200, 1001, 200))
+          ax_top.tick_params(labelbottom=False)
+
+          # Annotations
+          label_index = (rr * 3) + cc
+          ax_top.set_title(f'({chr(97 + label_index)})', loc='left', fontweight='bold')
+          ax_top.set_title(TTLS[ixh], fontsize=20, pad=10)
+
+   # --- CLEAN COLORBAR ROUTING ---
+   # 1. Absolute Control Colorbars (Stacked on the far right column)
+   cax_atmo_ctl = fig.add_subplot(gs[0, 3])
+   fig.colorbar(cf_atmo_ctl, cax=cax_atmo_ctl, label='CTL Atmo MSE [J/kg]')
+
+   cax_ocn_ctl = fig.add_subplot(gs[1, 3])
+   fig.colorbar(cf_ocn_ctl, cax=cax_ocn_ctl, label='CTL Ocean Temp [°C]')
+
+   # 2. Shared Anomaly Colorbars (Spanning columns 0 to 2 along the bottom row)
+   # Split the bottom slot into two horizontal tracks using subgridspec
+   gs_bottom_cbars = gs[2, 0:3].subgridspec(1, 2, wspace=0.4)
    
+   cax_atmo_anom = fig.add_subplot(gs_bottom_cbars[0, 0])
+   fig.colorbar(cf_atmo_anom, cax=cax_atmo_anom, orientation='horizontal', label='Anom Atmo MSE Anomaly [J/kg]')
+
+   cax_ocn_anom = fig.add_subplot(gs_bottom_cbars[0, 1])
+   fig.colorbar(cf_ocn_anom, cax=cax_ocn_anom, orientation='horizontal', label='Anom Ocean Temp Anomaly [°C]')
+
+
    # Assuming the output commands remain the same
    plt.savefig('thermo_state_plt.svg', bbox_inches='tight')
    plt.close()
