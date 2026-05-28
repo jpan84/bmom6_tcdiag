3edec3235e5a8c8b856ddc369a51127e05a41e0d
Joshua Pan <jvp5930@psu.edu>
2026-05-28 08:39:48 -0600

    adapted ux zonal mean to accept different tapes

--- paper1_post/preprocess/ux_zonmean.py
+++ paper1_post/preprocess/ux_zonmean.py
@@ -1,3 +1,4 @@
+import re
 import os
 import sys
 #sys.path.append('/glade/u/home/jpan/aquaptc/bmom6_tcdiag/paper1_post/')
@@ -6,15 +7,22 @@ import uxarray as ux
 import xarray as xr
 from dask.diagnostics import ProgressBar
 
-DIRI = sys.argv[1]
-TAPE = '*h0a.00[0-9]*.nc' #file names to process
+print(sys.argv)
+PTHPTR = sys.argv[1]#.replace('\\', '')
+HPTH = ''
+with open(PTHPTR, 'r') as f:
+   HPTH = f.read().strip()
+DIRI = os.path.dirname(HPTH)
+TAPE = re.findall(r'h[0-9][ia]', HPTH)
+#TAPE = '*h0a.00[0-9]*.nc' #file names to process
+
 UGRD = sys.argv[2]
 CONS = (sys.argv[3] == 'True') #conservative or not
 VARS = sys.argv[4] #example of comma-separated (no space): PS,PRECT,QFLX
 
 LATS = (-90, 90, 0.25)
 
-ds = ux.open_mfdataset(UGRD, os.path.join(DIRI, TAPE))
+ds = ux.open_mfdataset(UGRD, HPTH) #os.path.join(DIRI, TAPE))
 outds = None
 myvars = [str(dv) for dv in ds.data_vars]
 if VARS != 'all':
@@ -28,6 +36,6 @@ for dv in myvars:
       outds = outds.assign(variables={dv: zm})
 
 outds = outds.assign_attrs(script_from=sys.argv[0], conservative='True' if CONS else 'False', zmlats=str(LATS), ugrd=UGRD, infiles=TAPE)#, zonal_mean=str(outds.attrs['zonal_mean']))
-nameflags = ['uxzm', ('' if CONS else 'non') + 'cons'] + [str(itm) for itm in LATS] + ([] if VARS == 'all' else myvars)
+nameflags = ['uxzm', TAPE, ('' if CONS else 'non') + 'cons'] + [str(itm) for itm in LATS] + ([] if VARS == 'all' else myvars)
 with ProgressBar():
    outds.to_netcdf(os.path.join(DIRI, '..', '_'.join(nameflags) + '.nc'))
--- paper1_post/preprocess/zonmean_driver.py
+++ paper1_post/preprocess/zonmean_driver.py
@@ -5,11 +5,18 @@ from paths import ARCHRT, CAMGR
 import subprocess
 import signal
 
-VARS = 'Z3,T,Q'
+#VARS = 'Z3,T,Q'
+VARS = sys.argv[1]
+TAPE = sys.argv[2] if len(sys.argv) > 2 else 'atm/hist/*h0a.00[0-9]*.nc' #path and file names to process
 CONS = True
 
 for ii, ar in enumerate(ARCHRT):
-   hpth = os.path.join(ar, 'atm/hist/')
+   hpth = os.path.join(ar, TAPE)
+   #esc_hpth = hpth.replace('*', r'\*').replace('[', r'\[').replace(']', r'\]') #attempted to prevent bash from expanding wildcards
+   pthptr = f'zmdriver.pthptr{ii}'
+   with open(pthptr, 'w') as f:
+      f.write(hpth)
 
    proc = subprocess.Popen(f"qcmd -q casper -l walltime=01:21:00 -l select=1:ncpus=16:mem=256GB -A UCIS0005 python3 -u ux_zonmean.py\
-                    {hpth} {CAMGR} {str(CONS)} {VARS} &> zmdriver.out{ii}", shell=True)
+                    {pthptr} {CAMGR} {str(CONS)} {VARS} &> zmdriver.out{ii}", shell=True)
+   print(proc.args)
