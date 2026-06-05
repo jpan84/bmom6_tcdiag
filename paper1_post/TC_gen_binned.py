b8adc58e1a93b9005b12a14d5548178f0cd64d79
Joshua Pan <jvp5930@psu.edu>
2026-06-05 09:55:01 -0600

    cleaning up the plot config

--- paper1_post/TC_gen_binned.py
+++ paper1_post/TC_gen_binned.py
@@ -1,4 +1,4 @@
-from paths import CASENAMES, IXHORS
+from paths import CASENAMES, IXHORS, CTLIX
 import pandas as pd
 import numpy as np
 from scipy.stats import binned_statistic_2d
@@ -9,6 +9,9 @@ TRAJPQS = ['TC_preprocess/trajectories.txt.%s.parquet' % lc for lc in CASENAMES]
 
 DOY_GE = np.arange(0.5, 366, 1)
 MU_GE = np.linspace(-1, 1, 200) #mu=sinlat
+
+DOY_GE = np.arange(0.5, 366, 7)
+MU_GE = np.linspace(-1, 1, 40) #mu=sinlat
 
 MU2LAT = lambda x: np.rad2deg(np.arcsin(np.clip(x, -1., 1.)))
 LAT2MU = lambda x: np.sin(np.deg2rad(np.clip(x, -90., 90.)))
@@ -22,6 +25,14 @@ MU_GC = GE2GC(MU_GE)
 
 def main():
    dfs = [pd.read_parquet(tp) for tp in TRAJPQS]
+   for df in dfs:
+      if type(df.index) != pd.core.indexes.datetimes.DatetimeIndex:
+         df['dt'] = pd.to_datetime(df['dt'])
+         df.set_index('dt', inplace=True)
+   trange = [df.index.max() - df.index[0] for df in dfs]
+   totyrs = [tr.total_seconds() / 86400 / 365 for tr in trange]
+   print(totyrs)
+
    gen = [df[df['isgen']] for df in dfs]
    print(gen[0])
 
@@ -30,13 +41,20 @@ def main():
              for gdf in gen]
    #genct, _, _, binix
 
+   plt.rcParams['figure.figsize'] = (24, 10)
    fig, axes = plt.subplots(2, 3, sharex=True, sharey=True)
 
    for ii, ax in enumerate(axes.ravel()):
       if ii in [3, 4]: continue
       ixh = IXHORS[ii] - 1
-      print(binout[ixh][0].max())
-      ax.pcolormesh(DOY_GC, LAT_GC, binout[ixh][0].T, shading='nearest')
+      ctlix = CTLIX - 1
+      isctl = ixh == ctlix
+      pltar = binout[ixh][0]
+      if not isctl:
+         pltar -= binout[ctlix][0]
+      #ax.pcolormesh(DOY_GC, LAT_GC, binout[ixh][0].T, shading='nearest')
+      csf = ax.contourf(DOY_GC, LAT_GC, pltar.T, cmap='inferno' if isctl else 'bwr')
+      cb = plt.colorbar(csf, ax=ax)
 
    plt.show()
 
