c4203b3732cd713ffd297f6e85fc415a08d9dd5a
Joshua Pan <jvp5930@psu.edu>
2026-06-09 07:53:51 -0600

    all datastreams incorporated into new TC gen plots

--- paper1_post/TC_gen_binned.py
+++ paper1_post/TC_gen_binned.py
@@ -1,11 +1,14 @@
-from paths import CASENAMES, IXHORS, CTLIX
+import os
+from paths import CASENAMES, IXHORS, CTLIX, ARCHRT
+import cftime
 import pandas as pd
 import numpy as np
+import xarray as xr
 from scipy.stats import binned_statistic_2d
 import matplotlib.pyplot as plt
 
 TRAJPQS = ['TC_preprocess/trajectories.txt.%s.parquet' % lc for lc in CASENAMES] [1:]
-# Grid resolution for KDE evaluation
+SSTZM = 'atm/uxzm_h1i_noncons_-20.0_20.0_0.25_SST.nc'
 
 DOY_GE = np.arange(0.5, 366, 1)
 MU_GE = np.linspace(-1, 1, 200) #mu=sinlat
@@ -22,6 +25,8 @@ LAT_GC = GE2GC(LAT_GE)
 DOY_GC = GE2GC(DOY_GE)
 MU_GC = GE2GC(MU_GE)
 
+TICKDATES = [cftime.datetime(1, ii, 1, calendar='noleap') for ii in range(1, 13)]
+TICKDOY = [int(dt.strftime('%-j')) for dt in TICKDATES]
 
 def main():
    dfs = [pd.read_parquet(tp) for tp in TRAJPQS]
@@ -41,6 +46,30 @@ def main():
              for gdf in gen]
    #genct, _, _, binix
 
+   sstdss = []
+   for ar in ARCHRT:
+      #print(os.path.join(ar, SSTZM))
+      try:
+         sstdss.append(xr.open_dataset(os.path.join(ar, SSTZM)))
+      except:
+         sstdss.append(None)
+   #print(sstdss)
+
+   maxes = [None if ds is None else ds['SST'].idxmax('latitudes') for ds in sstdss]
+   del maxes[0]
+   #print(maxes)
+   #exit()
+   #sgn_chg = np.sign(maxes).diff(dim='time')
+   #chg_idx = sgn_chg['time'].where(sgn_chg != 0, drop=True)
+   #half_year = (chg_idx.dt.month > 6).astype(int)
+   #grp_key = chg_idx.dt.year + half_year * 0.5
+   #chg_idx.coords["half_years"] = grp_key
+   ##print(chg_idx)
+   #first_flip = chg_idx.groupby('half_years').first()
+   ##print(first_flip)
+   #flipdoys = first_flip.dt.dayofyear
+   #sprg, wntr = flipdoys.where(flipdoys > 182, drop=True).mean(), flipdoys.where(flipdoys <= 182, drop=True).mean()
+
    plt.rcParams['figure.figsize'] = (24, 10)
    fig, axes = plt.subplots(2, 3, sharex=True, sharey=True)
 
@@ -50,13 +79,39 @@ def main():
       ctlix = CTLIX - 1
       isctl = ixh == ctlix
       pltar = binout[ixh][0]
+
+      pltts = maxes[ixh] #None if maxes[ixh] is None else maxes[ixh]['SST'].idxmax('latitudes')
+      sp, wn = av_SST_flip_doy(pltts)
+
+      clevs = np.arange(0, 9) if isctl else np.arange(-8, 9)
       if not isctl:
          pltar -= binout[ctlix][0]
       #ax.pcolormesh(DOY_GC, LAT_GC, binout[ixh][0].T, shading='nearest')
-      csf = ax.contourf(DOY_GC, LAT_GC, pltar.T, cmap='inferno' if isctl else 'bwr')
+      csf = ax.contourf(DOY_GC, LAT_GC, pltar.T, cmap='inferno' if isctl else 'bwr', levels=clevs)
       cb = plt.colorbar(csf, ax=ax)
+      ax.scatter(pltts['time'].dt.dayofyear, pltts, alpha=0.2, c='lime', s=0.5) if pltts is not None else None
+      [ax.axvline(fd, c='gray', linestyle='dashed') if fd is not None else None for fd in [sp, wn]]
+
+      ax.set_ylim(-30, 30)
+      ax.set_xticks(TICKDOY, [dt.strftime('%m-%d') for dt in TICKDATES], rotation=45)
+      ax.tick_params(axis='both', labelleft=True, right=True, top=True)
 
    plt.show()
+
+def av_SST_flip_doy(maxlat):
+   if maxlat is None: return None, None
+
+   sgn_chg = np.sign(maxlat).diff(dim='time')
+   chg_idx = sgn_chg['time'].where(sgn_chg != 0, drop=True)
+   half_year = (chg_idx.dt.month > 6).astype(int)
+   grp_key = chg_idx.dt.year + half_year * 0.5 
+   chg_idx.coords["half_years"] = grp_key
+   #print(chg_idx)
+   first_flip = chg_idx.groupby('half_years').first()
+   #print(first_flip)
+   flipdoys = first_flip.dt.dayofyear
+   sprg, wntr = flipdoys.where(flipdoys > 182, drop=True).mean(), flipdoys.where(flipdoys <= 182, drop=True).mean()
+   return sprg, wntr
 
 if __name__ == '__main__':
    main()
