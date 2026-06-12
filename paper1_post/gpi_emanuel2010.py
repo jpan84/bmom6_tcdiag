78fb800f0aaeda6687757f313338b040d5d97936
Joshua Pan <jvp5930@psu.edu>
2026-06-12 07:47:06 -0600

    drafted relvort using apply_ufunc an uxarray core gradient func

--- paper1_post/gpi_emanuel2010.py
+++ paper1_post/gpi_emanuel2010.py
@@ -1,4 +1,5 @@
 import uxarray as ux
+from uxarray.core import gradient
 import xarray as xr
 import numpy as np
 
@@ -18,7 +19,7 @@ pintp = lambda x, p: x.interp(plev=p).drop('plev')
 def main():
    ds = ux.open_mfdataset(CAMGR, TSTFIL)
    ds = ds.sortby('plev', ascending=False)
-   ds = ds.chunk({'plev': -1, 'n_face': 777600 / 6 / 4})
+   ds = ds.chunk({'time': 1, 'plev': -1, 'n_face': -1})
    print(ds)
 
    SSTC = ds['TS'] - TCK
@@ -28,6 +29,7 @@ def main():
    rwv = ds['Q'] / (1 - ds['Q']) * 1000
 
    #PI vmax
+   print('PI task graph...')
    vmax, _, ifl, _, _ = pi_xr_ux(SSTC, MSL, phPa, TC, rwv)
    vmax = ux.UxDataArray(vmax, uxgrid=ds.uxgrid)
    vmax_zm = vmax.zonal_mean(lat=(-50, 50, 2)).squeeze()
@@ -38,6 +40,7 @@ def main():
    #plt.show()
 
    #Emanuel entropy deficit
+   print('Chi task graph...')
    rhb = q2rh(ds['PS'], ds['TREFHT'], ds['QREFHT'])
    pm = 6e4 #Pa, mid-tropo reference level
    Tm = pintp(ds['T'], pm)
@@ -49,17 +52,20 @@ def main():
    #print(chi_ent.values)
 
    #850 absolute vort
+   print('Vort task graph...')
    #rv = pintp(ds['U'], 8.5e4).curl(pintp(ds['V'], 8.5e4))
    rv = apply_ux_curl(pintp(ds['U'], 8.5e4), pintp(ds['V'], 8.5e4))
-   rv = ux.UxDataArray(rv, uxgrid=ds.uxgrid)
+   #rv = ux.UxDataArray(rv, uxgrid=ds.uxgrid)
    #print(rv.max().values)
-   va = rv + 2 * OM * np.sin(np.deg2rad(ds['lat'])).clip(-5e-5, 5e-5)
+   va = (rv + 2 * OM * np.sin(np.deg2rad(ds['lat']))).clip(-5e-5, 5e-5)
 
    #250-850 shear magnitude
+   print('Shear task graph...')
    ushr = pintp(ds['U'], 2e4) - pintp(ds['U'], 8.5e4)
    vshr = pintp(ds['V'], 2e4) - pintp(ds['V'], 8.5e4)
    shrmag = np.sqrt(ushr ** 2 + vshr ** 2)
 
+   print('GPI task graph...')
    f1 = np.abs(va) ** 3
    f2 = chi_ent ** (-4 / 3)
    f3 = np.maximum(vmax, 0) ** 2
@@ -68,6 +74,7 @@ def main():
    gpi = ux.UxDataArray(gpi, uxgrid=ds.uxgrid)
    gpi_zm = gpi.zonal_mean(lat=(-50, 50, 2)).squeeze()
 
+   print('Computing and plotting...')
    plt.plot(gpi_zm.latitudes, gpi_zm)
    plt.show()
 
@@ -83,13 +90,50 @@ def pi_xr_ux(*piargs):
 def ux_curl_oop(uu, vv):
    return uu.curl(vv)
 
-def apply_ux_curl(uu, vv):
-   return xr.apply_ufunc(ux_curl_oop, uu, vv,\
-            input_core_dims=[['n_face'] for _ in range(2)],\
-            output_core_dims=[['n_face']],\
-            vectorize=True, dask='parallelized',\
-            dask_gufunc_kwargs=dict(allow_rechunk=True),\
-            output_dtypes=[uu.dtype])
+#uu, vv should have the same coords (time, n_face)
+#arrays must not be chunked along dim n_face
+def apply_ux_curl(uu, vv, uxgrid):
+   face_coords = np.array(
+       [uxgrid.face_x.values, uxgrid.face_y.values, uxgrid.face_z.values]
+   ).T
+   face_lat = uxgrid.face_lat.values
+   face_lon = uxgrid.face_lon.values
+
+   node_coords = np.array(
+       [uxgrid.node_x.values, uxgrid.node_y.values, uxgrid.node_z.values]
+   )
+
+   face_lon_rad = np.deg2rad(face_lon)
+   face_lat_rad = np.deg2rad(face_lat)
+   normal_lat = np.array(
+       [
+           -np.cos(face_lon_rad) * np.sin(face_lat_rad),
+           -np.sin(face_lon_rad) * np.sin(face_lat_rad),
+           np.cos(face_lat_rad),
+       ]
+   ).T
+   normal_lon = np.array(
+       [
+           -np.sin(face_lon_rad),
+           np.cos(face_lon_rad),
+           np.zeros_like(face_lon_rad),
+       ]
+   ).T
+
+   coordargs = (uxgrid.n_face, face_coords, uxgrid.edge_face_connectivity.values,\
+                uxgrid.face_node_connectivity.values, uxgrid.node_edge_connectivity.values,\
+                face_lat, face_lon, node_coords, normal_lon, normal_lat)
+
+   callgrad = lambda da: xr.apply_ufunc(gradient._compute_gradients_on_faces, da, *coordargs,\
+                 input_core_dims=[['n_face']] + [[] for _ in range(len(coordargs))],\
+                 output_core_dims=[['n_face'], ['n_face']], vectorize=True,\
+                 dask='parallelized', output_dtypes=[da.dtype, da.dtype])
+
+   _, u_y = callgrad(uu)
+   v_x, _ = callgrad(vv)
+
+   return ux.UxDataArray(v_x - u_y, uxgrid=uxgrid)
+
 
 if __name__ == '__main__':
    main()
